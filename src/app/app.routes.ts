be10c869bc8fe2ec3025b0f62534df32150db043
Sparsh Codes <64547063+sparshsingh21@users.noreply.github.com>
2025-05-24 19:14:25 +0530

    Added routing and forced content to fit in 100vh (#1)
    * Added routing and forced content to fit in 100vh
    
    * refactor: remove unused component styles and test files
    
    * refactor: remove unused styleUrl properties from components
    
    ---------
    
    Co-authored-by: Anit Singh <anitsingh367@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { OrganisersComponent } from './components/organisers/organisers.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'organisers',
+    component: OrganisersComponent,
+  },
+];
--- src/app/components/info-card/info-card.component.ts
+++ src/app/components/info-card/info-card.component.ts
@@ -6,7 +6,6 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './info-card.component.html',
-  styleUrl: './info-card.component.css'
 })
 export class InfoCardComponent {
   @Input() title!: string;
